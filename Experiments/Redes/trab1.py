c5b679e4e3c0f1a4ae8cc73a1ce242745f19a682
fushigishi <lfdorelli@gmail.com>
2015-10-18 20:55:07 -0300

    Trab de redes

--- Experiments/Redes/trab1.py
+++ Experiments/Redes/trab1.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy as sp
 from scipy import stats
 import matplotlib.pyplot as plt
-import amon_analysis 
+import amon_analytics as an 
 import os
 
 net_file = sys.argv[1]
@@ -21,6 +21,7 @@ for x in cmps:
 	if cmps[x] not in cnt:
 		cnt[cmps[x]] = 0
 	cnt[cmps[x]] += 1
+
 bst = -1
 for x in cnt:
 	if bst == -1:
@@ -36,20 +37,19 @@ big = g.filter_nodes(filt)
 
 clust = []
 degs = []
-for i in range(0, big.nodes_qty()):
+
+for i in big.node_keys():
 	degs.append(len(big.adjacency(i)))
 	cc = big.local_clustering(i)
 	clust.append(cc)
-
-# Display the degree histogram
-amon_analysis.degree_histogram(degs, 300)
 
 # Get the shortest paths (max + avg)
 diam = 0.0
 avg_path = 0
 den = 0.0
 eff = 0.0
-for i in range(0, big.nodes_qty()):
+
+for i in big.node_keys():
 	dst = big.bfs(i)
 	for j in dst:
 		if j == i:
@@ -61,17 +61,36 @@ for i in range(0, big.nodes_qty()):
 eff /= den
 avg_path /= den
 
+
 pos = net_file.find("out.")
 net = "results/" + net_file[pos+4:]
-net_n = "results/" + net_file[pos+4:]  + "_degree.png"
-# print net_n
-plt.tight_layout()
-plt.savefig(net_n, dpi=200)
-# plt.show()
+net_n = "results/" + net_file[pos+4:] 
+
+# Saves the fitted degree distribution
+l, k = an.power_law_fit(degs)
+# print l, k
+
+plt.savefig(net_n +  "_dfit.pdf", dpi=200)
+
+# Saves degree distribution
+an.degree_histogram(degs, True, True, False)
+plt.savefig(net_n +  "_degree.pdf", dpi=200)
+
+#  Saves accum degree 
+an.degree_histogram_complementar(degs)
+plt.savefig(net_n +  "_degree_compl.pdf", dpi=200)
+
+# Saves clustering distribution
+an.local_cc_acc(clust)
+plt.savefig(net_n +  "_clust.pdf", dpi=200)
+
+# local clustering x degree
+an.local_cc_deg_distr(degs, clust)
+plt.savefig(net_n +  "_cc_deg.pdf", dpi=200)
 
 
-
-print  ' & ', round(big.average_degree(), 3), ' & ', round(stats.moment(degs, 2), 3), ' & ', round(stats.entropy(degs), 3), round(np.average(clust), 3), ' & ', round(big.global_cc(), 3), ' & ',  round(avg_path, 3), ' & ', round(diam, 3), ' & ' , round(eff, 4)
+# Prints the table 
+print  ' & ', round(big.average_degree(), 3), ' & ', round(stats.moment(degs, 2), 3), ' & ', round(stats.entropy(degs), 3), ' & ', round(np.average(clust), 3), ' & ', round(big.global_cc(), 3), ' & ',  round(avg_path, 3), ' & ', round(diam, 3), ' & ' , round(eff, 4), ' \\\\hline'
 
 s = big.as_dot(False)
 f = open(net + '.dot', 'w+')
--- amon_analytics/build/lib.linux-x86_64-2.7/amon_analytics.py
+++ amon_analytics/build/lib.linux-x86_64-2.7/amon_analytics.py
@@ -1,12 +1,169 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import os
+import math
 
+
+def rng_powerlaw(xmin, a, size=1):
+	nr = np.random.random(size)
+	res = []
+	for r in nr:
+		val = math.floor( (xmin - 0.5) * ((1. - r)**(-1.0/(a-1.))) + .5)
+		res.append(val)
+	return res
+
+
+def power_law_lambda(kmin, degs):
+	s = 0.0
+	for x in degs:
+		s += math.log1p(float(x)/float(kmin))
+	return 1.0 + float(len(degs))/s;
+
+def distr(data):
+	cnt = {}
+	n = len(data)
+	for x in data:
+		if x not in cnt:
+			cnt[x] = 0
+		cnt[x] += 1
+	for x in cnt:
+		cnt[x] /= float(n)
+	return cnt
+
+def cumul(data):
+	cnt = {}
+	n = len(data)
+	for x in data:
+		if x not in cnt:
+			cnt[x] = 0
+		cnt[x] += 1
+
+	c = 0
+	for x in sorted(cnt):
+		c += cnt[x]
+		cnt[x] = float(c)/float(n)		
+	return cnt
+
+def plot_with_fit(data, fit, kmin, cumul=False):
+	plt.clf()
+
+	fig, ax = plt.subplots(1, 1, figsize=(8,8))
+
+	x = []
+	y = []
+	xf = []
+	yf = []	
+	mx = 0
+	my = 1
+	for k in sorted(data):
+		x.append(k)
+		mx = max(mx, k)
+		my = min(my, data[k])
+		y.append(data[k])
+
+	for k in sorted(fit):
+		xf.append(k)
+		yf.append(fit[k])
+
+	# ax.set_yscale('log')
+	ax.set_xscale('log')
+	ax.set_xlim(kmin, mx)
+	ax.set_ylim(my, 1)
+
+	ax.set_xlabel(r'Degree $ (k)$', size=25)
+	ax.set_ylabel(r'$P (k \leq K)$', size=25)
+
+	ax.plot(xf, yf, color='red', label='Fitted', linestyle='--')
+	if cumul:
+		ax.plot(x, y, color='black', label='Original', drawstyle='steps')	
+	else:
+		ax.scatter(x, y, marker='o', label='Original', facecolors='none', edgecolors='black', s=16.0)		
+	handles, labels = ax.get_legend_handles_labels()
+	ax.legend(handles, labels, loc=4)
+
+def power_law_fit(data):
+	bst = 1000.0
+	n = len(data)
+	m = np.max(data)
+	S = cumul(data)
+	D = {}
+	d = []
+	L = 0
+	KM = 0
+
+	mx = 0
+	for x in data:
+		mx = max(mx, x)
+
+	for kmi in range(1, min(10, mx/2)):
+		l = power_law_lambda(kmi, data)
+
+		ddata = rng_powerlaw(kmi, l, 5000)
+		P = cumul(ddata)
+		r = 0.0
+		for x in S:
+			if x in P:
+				r = max(r, abs(S[x] - P[x]))		
+		if r < bst:
+			bst = r
+			D = P
+			L = l
+			KM = kmi
+			d = ddata
+
+	plot_with_fit(S, D, KM, True)
+	return L, KM
+
+
+def local_cc_deg_distr(degs, clust):
+	plt.clf()
+	fig, ax = plt.subplots(1, figsize=(8,8))
+
+	xv = []
+	yv = []
+	mdeg = 0
+	for i in range(0, len(degs)):
+		if clust[i] > 0.0:
+			xv.append(degs[i])
+			yv.append(clust[i])
+			mdeg = max(mdeg, degs[i])
+
+	ax.scatter(xv, yv, marker='o', facecolors='none', edgecolors='black', s=16.0)
+	ax.plot([1, mdeg], [1.0, 0.001], linestyle='--', color='black')
+	plt.xlabel('Degree $(k)$', size=25)
+	plt.ylabel('Local clustering coefficient $C(k)$', size=25)
+	plt.xscale('log')
+	plt.yscale('log')	
+	ax.set_xlim(1, mdeg + 10)
+	ax.set_ylim(0.001, 1.000)
+	ax.set_yticks((0.01, 0.1, 1.0))
+
+def local_cc_acc(data):
+	plt.clf()
+	mxv = len(data)
+	cnt = {}
+	for x in data:
+		if x not in cnt:
+			cnt[x] = 0
+		cnt[x] += 1
+
+	dx = []
+	dy = []
+	C = cumul(data)
+	for x in sorted(C):
+		dx.append(x)
+		dy.append(C[x])
+
+	plt.plot (dx, dy, color='black',  drawstyle='steps')
+	plt.xlabel('Local clustering coefficient $(c)$', size=25)
+	plt.ylabel('$P(x \leq c)$', size=25)
+	# plt.yscale('log')
 
 def degree_histogram(data, logX=True, logY=True, tight=False):	
-	
-	plt.figure()
+	plt.clf()
+	# plt.figure()
 	maxv = 0
 	cnt = {}
 	for x in data:
@@ -27,10 +184,10 @@ def degree_histogram(data, logX=True, logY=True, tight=False):
 		maxy = max(maxy, y)
 		minv = min(minv, y)
 
-	plt.ylabel('Frequency')
-	plt.xlabel('Degree')
+	plt.ylabel('Frequency', size=25)
+	plt.xlabel('Degree', size=25)
 	
-	plt.scatter(xpos, yvs, color='blue', marker='x')
+	plt.scatter(xpos, yvs,  marker='o', facecolors='none', edgecolors='black')
 	
 	if tight == True:
 		plt.xlim([1, maxv])
@@ -41,7 +198,7 @@ def degree_histogram(data, logX=True, logY=True, tight=False):
 		plt.yscale('log')	
 
 def degree_histogram_complementar(data):	
-	plt.figure()
+	plt.clf()
 	maxv = 0
 	cnt = {}
 	for x in data:
@@ -60,7 +217,6 @@ def degree_histogram_complementar(data):
 		maxy = max(maxy, y)
 		minv = min(minv, y)
 
-	plt.title('Cumulative Distribution')
 	plt.xscale('log')
 	cumulative = []
 	c = 0
@@ -77,11 +233,9 @@ def degree_histogram_complementar(data):
 		minv = min(yvs[i], minv)
 	
 	plt.yscale('log')
-	plt.ylabel('P (d >= x)')
-	plt.xlabel('Degree (d)')
+	plt.ylabel('$P (d \geq x)$', size=25)
+	plt.xlabel('Degree (d)', size=25)
 	plt.xlim([1, maxv])
 	plt.ylim([minv, 1])
 	plt.legend()
-	l2, = plt.plot(xpos, yvs, color='blue', label='cumulative', drawstyle='steps')
-
-
+	l2, = plt.plot(xpos, yvs, color='black', label='cumulative', drawstyle='steps')
